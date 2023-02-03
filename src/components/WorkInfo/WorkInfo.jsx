143f198caa39310e22fa2ca3f12d78a4c0bf1bb4
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-02-02 23:05:58 -0500

    push restaurant project ixed

--- src/components/WorkInfo/WorkInfo.jsx
+++ src/components/WorkInfo/WorkInfo.jsx
@@ -8,7 +8,7 @@ import Chiwi from "../../assets/chiwi.jpg";
 import Snake from "../../assets/snake.jpg";
 import Menta from "../../assets/menta.jpg";
 import Vased from "../../assets/vased.jpg";
-import Restaurant from "../../assets/restaurant.jpg";
+import Restaurant from "../../assets/restaurant.jpeg";
 
 export const projectsData = [
   {
@@ -20,34 +20,41 @@ export const projectsData = [
   },
   {
     id: 2,
+    image: Restaurant,
+    title: "Gericht restaurant",
+    category: "web",
+    deploy: "https://react-restaurant-lord-mugen.vercel.app/",
+  },
+  {
+    id: 3,
     image: News,
     title: "News Homepage",
     category: "web",
     deploy: "https://news-homepage-ivory-eight.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     image: Dom,
     title: "Toquen el dom",
     category: "web",
     deploy: "http://luiscabal.me/Toquen-el-DOM/",
   },
   {
-    id: 4,
+    id: 5,
     image: Avia,
     title: "Avia Test",
     category: "web",
     deploy: "https://avia-test-three.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     image: Aranda,
     title: "Aranda Test",
     category: "web",
     deploy: "https://aranda-test.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     image: Portfolio1,
     title: "First portfolio design",
     category: "design",
@@ -55,7 +62,7 @@ export const projectsData = [
       "https://www.figma.com/file/IACOycU4H5H95hLoThctlQ/Portfolio?node-id=0%3A1&t=6tLAvNvfnQ6aRZAR-1",
   },
   {
-    id: 7,
+    id: 8,
     image: Chiwi,
     title: "Chiwi academy design",
     category: "design",
@@ -63,28 +70,21 @@ export const projectsData = [
       "https://www.figma.com/file/Jh1CKBC7b7NJh95VJB0E1e/Chiwi-academy?node-id=0%3A1&t=osQ7I6NnYNPk8aBn-1",
   },
   {
-    id: 8,
+    id: 9,
     image: Snake,
     title: "Python snake game",
     category: "app",
     deploy: "https://github.com/Lord-Mugen/Snake-Game",
   },
   {
-    id: 9,
+    id: 10,
     image: Menta,
     title: "Menta y violeta design",
     category: "design",
     deploy:
       "https://www.figma.com/file/rKKO0FhoKH3xBORbZYlWzG/Mentta-y-Violetta?node-id=0%3A1&t=CqJ43MF5jVkObPHo-1",
   },
-  {
-    id: 10,
-    image: Dom,
-    title: "Toquen el dom design",
-    category: "design",
-    deploy:
-      "https://www.figma.com/file/bOrqJ6uJKM3uYF1NiQ2XEU/Aplicaci%C3%B3n-Web-M%C3%BAsical?node-id=0%3A1&t=b6VIjpHTxKcC44oz-1e",
-  },
+
   {
     id: 11,
     image: Vased,
@@ -95,17 +95,18 @@ export const projectsData = [
   },
   {
     id: 12,
+    image: Dom,
+    title: "Toquen el dom design",
+    category: "design",
+    deploy:
+      "https://www.figma.com/file/bOrqJ6uJKM3uYF1NiQ2XEU/Aplicaci%C3%B3n-Web-M%C3%BAsical?node-id=0%3A1&t=b6VIjpHTxKcC44oz-1e",
+  },
+  {
+    id: 13,
     image: Vased,
     title: "Vased school app",
     category: "app",
     deploy: "https://vased-web.vercel.app/",
-  },
-  {
-    id: 13,
-    image: Restaurant,
-    title: "Gericht restaurant",
-    category: "web",
-    deploy: "https://react-restaurant-lord-mugen.vercel.app/",
   },
 ];
 
