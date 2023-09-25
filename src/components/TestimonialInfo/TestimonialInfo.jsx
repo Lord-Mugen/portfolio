9fb454fc3e48a199f6c9ce8760db235c31df04dd
Christian ruiz <christianjavieruizcortes@gmail.com>
2023-09-24 21:01:04 -0500

    testimonial adding

--- src/components/TestimonialInfo/TestimonialInfo.jsx
+++ src/components/TestimonialInfo/TestimonialInfo.jsx
@@ -1,12 +1,13 @@
 import Image1 from "../../assets/ricardo-profile-pic.png";
 import Image2 from "../../assets/vased-profile-pic.jpeg";
 import Image3 from "../../assets/diana.jpg";
+import Image4 from "../../assets/shark.jpeg";
 
 export const Data = [
   {
     id: 1,
     image: Image1,
-    title: "Ricardo Cortés",
+    title: "Ricardo",
     description:
       "A good developer, does his job well, is a good teammate and is always learning something new and improving.",
   },
@@ -20,8 +21,14 @@ export const Data = [
   {
     id: 3,
     image: Image3,
-    title: "Laura Mendez",
+    title: "Laura",
     description:
       "He has a good performance and meets the requirements assigned during his work on time.",
+  },
+  {
+    id: 4,
+    image: Image4,
+    title: "Shark Technology",
+    description: "Good employee, with good performance and does good teamwork.",
   },
 ];
